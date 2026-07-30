87bc4c540a7151642b4444e81e0755d8bbaf26da
Masato Usami <uusan3581@gmail.com>
2026-07-31 04:47:07 +0900

    Add local project files

--- ESP32_Code/mainBoard/ClockReceiver/motor_test/motor_test.ino
+++ ESP32_Code/mainBoard/ClockReceiver/motor_test/motor_test.ino
@@ -17,10 +17,17 @@ void loop()
 {
     DEBUG_PRINTLN("Forward");
 
+<<<<<<< HEAD
     for(int i = 1; i <= 200; i++) {
       motorStepForward();
 
         DEBUG_PRINTF("Step = %d\n", 1);
+=======
+    for(int i = 1; i <= 218; i++) {
+      motorStepForward();
+
+        DEBUG_PRINTF("Step = %3d\n", i);
+>>>>>>> 91a6792 (First commit. Clock　Project　全てです。ESP32用コードも含まれています。SwitchBox制御用。MainBoard(Motor駆動用)。RS485通信。MainBoardはモーターテストコードまで。今後ホールセンサーを追加して、通信機能までコードを完成させていく予定です。)
 
       delay(20);
     }
@@ -30,10 +37,17 @@ void loop()
 
     Serial.println("Reverse");
 
+<<<<<<< HEAD
     for(int i = 1; i <= 200; i--) {
       motorStepReverse();
 
       DEBUG_PRINTF("Step = %d\n", 1);
+=======
+    for(int i = 1; i <= 218; i++) {
+      motorStepReverse();
+
+      DEBUG_PRINTF("Step = %3d\n", i);
+>>>>>>> 91a6792 (First commit. Clock　Project　全てです。ESP32用コードも含まれています。SwitchBox制御用。MainBoard(Motor駆動用)。RS485通信。MainBoardはモーターテストコードまで。今後ホールセンサーを追加して、通信機能までコードを完成させていく予定です。)
   
 
       delay(20);
